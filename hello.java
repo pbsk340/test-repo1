91384b226606ff023a15ce9a554a6b41a2ae8055
dev16 <dev1@mail.com>
2021-05-17 22:35:07 +0530

    file modf

--- hello.java
+++ hello.java
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 class hello    #from dev2
 {
-	public static void main()    #from dev2
-
-
-	{
-		system.out.println("hello from dev1");
-	}
-<<<<<<< HEAD
+	public static voidi main()    #from dev2
+        {
+		system.out.println("hello from");...modify
+        }
 }#from dev2
-=======
-
-
-}
->>>>>>> 0670cb11492bf61b1fd5623ca9c51d6dd7b741f9
